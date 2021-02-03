9b6ca1489b795c43b8c68938ec280630b3b99b21
Michael Walter <michael_patrick.walter@outlook.com>
2021-02-02 22:25:20 -0800

    Day-1: Caught up on and completed all content.

---
+++ Day-1/naming.py
@@ -0,0 +1,4 @@
+
+# When naming variables, choose meaningful names
+# camel case, snake case, or underscore can be used for this
+# Some words in python are reserved and shouldn't be used
---
+++ Day-1/project-bandNameGenerator.py
@@ -0,0 +1,14 @@
+# Day 1 project, Band Name Generator
+
+#1. Create a greeting for your program.
+print("Greetings, this program will ask you two questions and the combination of which will be your band name. Have Fun! \n")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in? \n  ")
+
+#3. Ask the user for the name of a pet.
+pet = input("What was the name of your first pet? \n  ")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print(f"Your band name is {city} {pet}")
